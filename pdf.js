f8dbf224000fd08fbe62a8a408eb848316e33c0f
krychuq1 <krys.nagorny@gmail.com>
2020-11-01 14:13:02 +0100

    initial build

--- pdf.js
+++ pdf.js
@@ -1,57 +1,68 @@
-const fs = require("fs");
-const path = require("path");
+require('dotenv').config()
+const fs = require('fs');
+const path = require('path');
 const puppeteer = require('puppeteer');
 const handlebars = require("handlebars");
-
-try {
-    (async () => {
-        var dataBinding = {
-            items: [{
-                name: "item 1",
-                price: 100
-            },
-            {
-                name: "item 2",
-                price: 200
-            },
-            {
-                name: "item 3",
-                price: 300
-            }
-            ],
-            total: 600,
-            isWatermark: true
+const axios = require('axios').default;
+class Pdf{
+    constructor() {
+        // token = '';
+        this.getToken();
+    }
+    async getToken() {
+        try {
+            const res = await axios.post(process.env.backend_url_dev + 'auth/login', {
+                username: process.env.admin_login,
+                password: process.env.admin_pw_dev
+            });
+            this.token = res.data.access_token;
+        } catch (e) {
+            console.log(e);
+            return e;
         }
+    }
+    async getOrders() {
 
-        var templateHtml = fs.readFileSync(path.join(process.cwd(), 'invoice.html'), 'utf8');
-        var template = handlebars.compile(templateHtml);
-        var finalHtml = encodeURIComponent(template(dataBinding));
-        var options = {
-            format: 'A4',
-            headerTemplate: "<p></p>",
-            footerTemplate: "<p></p>",
-            displayHeaderFooter: false,
-            margin: {
-                top: "40px",
-                bottom: "100px"
-            },
-            printBackground: true,
-            path: 'invoice.pdf'
-        }
-
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox'],
-            headless: true
-        });
-        const page = await browser.newPage();
-        await page.goto(`data:text/html;charset=UTF-8,${finalHtml}`, {
-            waitUntil: 'networkidle0'
-        });
-        await page.pdf(options);
-        await browser.close();
-
-        console.log('Done: invoice.pdf is created!')
-    })();
-} catch (err) {
-    console.log('ERROR:', err);
+    }
 }
+const pdf = new Pdf();
+
+
+
+
+
+// try {
+//     (async () => {
+//         var dataBinding = {}
+//         var templateHtml = fs.readFileSync(path.join(process.cwd(), 'template.html'), 'utf8');
+//         var template = handlebars.compile(templateHtml);
+//         var finalHtml = encodeURIComponent(template(dataBinding));
+//         var options = {
+//             format: 'A4',
+//             headerTemplate: "<p></p>",
+//             footerTemplate: "<p></p>",
+//             displayHeaderFooter: false,
+//             margin: {
+//                 top: "40px",
+//                 bottom: "100px"
+//             },
+//             printBackground: true,
+//             path: 'invoice.pdf'
+//         }
+//
+//         const browser = await puppeteer.launch({
+//             args: ['--no-sandbox'],
+//             headless: true
+//         });
+//         const page = await browser.newPage();
+//         await page.goto(`data:text/html;charset=UTF-8,${finalHtml}`, {
+//             waitUntil: 'networkidle0'
+//         });
+//         await page.pdf(options);
+//         await browser.close();
+//
+//         console.log('Done: invoice.pdf is created!')
+//     })();
+// } catch (err) {
+//     console.log('ERROR:', err);
+// }
